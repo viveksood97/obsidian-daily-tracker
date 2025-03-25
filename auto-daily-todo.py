3d47a3a13ed158bd78aa50d479bc6773e2d2d700
Vivek Sood <viveksood257@gmail.com>
2025-03-24 19:44:55 -0600

    Rand

--- auto-daily-todo.py
+++ auto-daily-todo.py
@@ -2,19 +2,28 @@
 
 import datetime
 import json
+import os
 
-def file_path(config):
+def create_dir_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def file_dir_and_name(config):
     now = datetime.datetime.now()
     year = now.strftime(config['year_format'])
     month = now.strftime(config['month_format'])
     date_time = now.strftime(config['date_time_format'])
-    return f"{config['path_to_daily_todos']}/{year}/{month}/{date_time}.md"
+    return f"{config['path_to_daily_todos']}/{year}/{month}", f"{date_time}.md"
 
 def main():
     with open('config.json') as config_file:
         config = json.load(config_file)
-
-    print(file_path(config))
+    file_dir, file_name = file_dir_and_name(config)
+    create_dir_if_not_exists(file_dir)
+    with open(file_dir + "/" + file_name, 'w') as file:
+        file.write(f"# {datetime.datetime.now().strftime(config['date_time_format'])}\n\n")
+        file.write(f"## Tasks\n\n")
+        file.write(f"## Notes\n\n")
 
 
 if __name__ == '__main__':
